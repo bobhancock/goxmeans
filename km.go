53a01e12df3bc397adfbb26a18f53bb17fb22fad
Robert Hancock <hancock.robert@gmail.com>
2012-06-08 18:08:49 -0400

    Consolidated funcs into km.go

--- km.go
+++ km.go
@@ -3,7 +3,7 @@
 
  See Dan Pelleg and Andrew Moore: X-means: Extending K-means with Efficient Estimation of the Number of Clusters. 
 */
-package gomeans
+package goxmeans
 
 import (
 	"fmt"
@@ -16,13 +16,45 @@ import (
 	"math"
 	"math/rand"
 	"code.google.com/p/gomatrix/matrix"
-	"matutil"
 )
 
 // Atof64 is shorthand for ParseFloat(s, 64)
 func Atof64(s string) (f float64, err error) {
 	f64, err := strconv.ParseFloat(s, 64)
 	return float64(f64), err
+}
+
+// ColSlice puts the values in column i of a matrix as a slice
+func ColSlice(mat matrix.Matrix, i int) []float64 {
+	rows, _ := mat.GetSize()
+	r := make([]float64, rows)
+	for j := 0; j <  rows; j++ {
+		r = append(r, mat.Get(j, i))
+	}
+	return r
+}
+
+// AppendCol appends column to and existing matrix.  If length of column
+// is greater than the number of rows in the matrix, and error is returned.
+// If the length of column is less than the number of rows, the column is padded
+// with zeros.
+func AppendCol(mat matrix.Matrix, column []float64) (*matrix.DenseMatrix, error) {
+	rows, cols := mat.GetSize()
+	err := errors.New("")
+	if len(column) > rows {
+		return matrix.Zeros(1, 1), errors.New(fmt.Sprintf("Cannot append a column with %d elements to an matrix with %d rows.",len(column),rows))
+	}
+	// Put the source array into a slice.
+	// If there are R rows and C columns, the first C elements hold the data in
+	// the first row, the 2nd C elements hold the data in the 2nd row, etc.
+	source := make([]float64, rows * cols + len(column))
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			source = append(source, mat.Get(i,j))
+		}
+		source = append(source, column[i])
+	}
+	return matrix.MakeDenseMatrix(source, rows, cols + 1), err
 }
 
 	
@@ -77,28 +109,30 @@ func Load(fname string) (*matrix.DenseMatrix, error)  {
 	return datamatrix, nil
 }
 
-// randcent picks random centroids 
-func RandCentroids(mat *matrix.DenseMatrix, k int) (matrix.DenseMatrix) {
+// RandCentroids picks random centroids based on the min and max values in the matrix.
+func RandCentroids(mat *matrix.DenseMatrix, k int) (*matrix.DenseMatrix, error) {
 	rows,cols := mat.GetSize()
-	minj := 0
+	centroids := matrix.Zeros(1,1)
+
+	minj := float64(0)
 	for j := 0; j <  cols; j++ {
-		r := matutil.ColSlice(mat, j)
-		// min value from each column
+		r := ColSlice(mat, j)
+		// min value from column
 		for _, val := range r {
-			minj := math.Min(minj, val)
+			minj = math.Min(minj, val)
 		}
 
-		// max value from each column
-		maxj := 0
-		for _,val = range r {
-			maxj := math.Max(maxj, val)
+		// max value from column
+		maxj := float64(0)
+		for _, val := range r {
+			maxj = math.Max(maxj, val)
 		}
 
 		// create a slice of random centroids 
-		// base on minj + rangeJ * random num based on k
+		// based on maxj + minJ * random num to stay in range
 		rands := make([]float64, rows)
 		for i := 0; i < rows; i++ {
-			rands = appeand(rands,  maxj - minj * rand.Float64())
+			rands = append(rands,  maxj - minj * rand.Float64())
 		}
 
 		centroids, err := AppendCol(mat, rands)
@@ -106,6 +140,6 @@ func RandCentroids(mat *matrix.DenseMatrix, k int) (matrix.DenseMatrix) {
 			return centroids, errors.New(fmt.Sprintf("means: RandCentroids could not append column.  err=%v", err))
 		}
 	}
-	return centroids
+	return centroids, nil
 }
 
