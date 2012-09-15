adfe3499d6125d4278db609e1029c5dc7aedbef7
Robert Hancock <hancock.robert@gmail.com>
2012-09-15 12:49:03 -0400

    Added functest and gendata

---
+++ functests/func.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"goxmeans"
+	"os"
+	"flag"
+	"runtime/pprof"
+	"log"
+	"strconv"
+)
+
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+//var memprofile = flag.String("memprofile", "", "write memory profile to this file")
+//var klower = flag.Int("klower", "", "klower bound"
+var c = flag.Int("centroids", 1234, "number of centroids")
+
+
+
+func main() {
+	usage := "usage: func numcentroids"
+	if len(os.Args) < 1 {
+		fmt.Println(usage)
+		return
+	}
+
+	k, _ := strconv.Atoi(os.Args[1])
+
+   flag.Parse()
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal(err)
+        }
+		pprof.StartCPUProfile(f)
+        defer pprof.StopCPUProfile()
+	}
+
+	data, err := goxmeans.Load("randints")
+	if err != nil {
+		fmt.Println("Load: ", err)
+		return
+	}
+	fmt.Println("Load complete")
+	fmt.Println(data.GetSize())
+
+	var measurer goxmeans.EuclidDist
+	//var cc goxmeans.DataCentroids
+	cc := goxmeans.EllipseCentroids{0.5}
+	bisectcc := goxmeans.EllipseCentroids{0.5}
+	models, errs := goxmeans.Xmean(data, k, cc, bisectcc, measurer)
+	if len(errs) > 0 {
+		for k, v := range errs {
+			fmt.Printf("%s: %v\n", k, v)
+		}
+	}
+	
+	fmt.Println("goxmeans complete")
+
+	for _, model := range models {
+		fmt.Printf("original num centroids=%d  bic=%f  numclusters=%d\n", model.Numcentroids, model.Bic, len(model.Clusters))
+		for i, clust := range model.Clusters {
+			fmt.Printf("cluster_id=%d numpoints=%d variance=%f  centroid=%v\n", i, clust.Numpoints(), clust.Variance, clust.Centroid)
+		}
+	}
+}
